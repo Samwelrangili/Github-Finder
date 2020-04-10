0b5cfa0db7ab11552670d7c60d3c04a9689f03b3
Samwelrangili <samwelrangili896@gmail.com>
2020-04-10 17:16:57 +0300

    created a profile component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileService} from './profile.service';
+import { ProfileComponent } from './profile/profile.component'
 
 @NgModule({
   declarations: [
     AppComponent,
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ProfileService
   ],
   providers: [],
   bootstrap: [AppComponent]
