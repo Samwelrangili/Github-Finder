c7c88f889a61bb832389ea6baacc83f580b6f641
Samwelrangili <samwelrangili896@gmail.com>
2020-04-11 16:05:40 +0300

    imported ProfileService from profile.service file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,15 @@ import { HttpClient,HttpHeaders }from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
-import { ProfileService } from './profile.service'
+import { ProfileService } from '../app/service/profile.service';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule }from '@angular/forms';
-// import { DateCountPipe } from './date-count.pipe';
-// import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
-    // DateCountPipe,
-    // NavbarComponent
   ],
 
   imports: [
