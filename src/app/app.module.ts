80d9d9861483ca32cba1769b82c685baea817dff
Samwelrangili <samwelrangili896@gmail.com>
2020-04-12 15:13:40 +0300

    generated component sampleusers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { ProfileComponent } from './profile/profile.component';
 import { ProfileService } from '../app/service/profile.service';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule }from '@angular/forms';
+import { SampleusersComponent } from './sampleusers/sampleusers.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
+    SampleusersComponent,
   ],
 
   imports: [
